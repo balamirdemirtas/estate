b3ea92515bc58869c286fd9e2b1efa077bf0abf6
balamirdemirtas <45852043+balamirdemirtas@users.noreply.github.com>
2022-01-31 21:59:19 +0300

    Update settings.py

--- estate/settings.py
+++ estate/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's_5kl_d82qt200zw3raz*sozs1v-c(+beayextlq%t4o0&np@1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
